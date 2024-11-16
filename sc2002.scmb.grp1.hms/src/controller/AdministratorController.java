48c89689f7602da6f69ea13f954cf921f9253c25
ZaydShabazAli <167412532+ZaydShabazAli@users.noreply.github.com>
2024-11-16 19:04:53 +0800

    AdminController methods

--- sc2002.scmb.grp1.hms/src/controller/AdministratorController.java
+++ sc2002.scmb.grp1.hms/src/controller/AdministratorController.java
@@ -5,24 +5,39 @@ import java.util.*;
 import java.util.Scanner;
 
 import entity.Administrator;
+import entity.MedicationInventory;
 import repository.AdministratorRepository;
 import controller.MedicationInventoryController;
 import controller.AppointmentController;
-
 
 public class AdministratorController {
     private final Scanner scanner = new Scanner(System.in);
     private MedicationInventoryController inventoryController = new MedicationInventoryController();
     private AppointmentController appointmentController = new AppointmentController();
 
+    public void addStock(String medicine, int amount, int level) throws IOException
+    {
+        inventoryController.addMedicine(medicine, amount, level);
+    }
+
+    public void removeStock(String medicine) throws IOException
+    {
+        inventoryController.removeMedicine(medicine);
+    }
+
     public void replenishStock(String medicine, int amount) throws IOException
     {
         inventoryController.replenishInventory(medicine, amount);
     }
 
-    public void viewAppointment() throws IOException
+    public void changeAlert(String medicine, int amount) throws IOException
     {
-        
+        inventoryController.updateAlert(medicine, amount);
+    }
+
+    public List<MedicationInventory> viewInventory() throws IOException
+    {
+        return inventoryController.inventory();
     }
 
     
